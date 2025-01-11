98fb1106f69849a7797d4cb2e9642790878bdd2e
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-01-11 16:14:32 +0530

    finish userSearch

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/Greeshmanth1909/boni_tools
 go 1.22.2
 
 require (
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/schollz/progressbar/v3 v3.18.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
@@ -13,5 +17,7 @@ require (
 	go.mongodb.org/mongo-driver v1.17.1 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 )
--- userSearchFiltering/main.go
+++ userSearchFiltering/main.go
@@ -1,27 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-	"time"
-    "context"
-    "encoding/json"
-    "regexp"
-    "go.mongodb.org/mongo-driver/bson"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/schollz/progressbar/v3"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
 )
 
-func main() {
-    fmt.Println("userSearchFiltering: accessing environment variable:- mongoUrl")
-    mongoUrl := os.Getenv("mongoUrl")
-    if mongoUrl == "" {
-        fmt.Println("Please set environment variable: mongoUrl to the url of mongoDB")
-        return
-    }
+type Mongo struct {
+	ConvStartMsg    string `bson:"conv_start_msg"`
+	BusinessDetails []struct {
+		BusinessName string `bson:"business_name"`
+		Location     string `bson:"location"`
+		PhoneNumber  string `bson:"phone_number"`
+	} `bson:"business_details"`
+}
 
-    clientOptions := options.Client().ApplyURI(mongoUrl)
+type Business struct {
+	BusinessName string `bson:"business_name"`
+	Location     string `bson:"location"`
+	PhoneNumber  string `bson:"phone_number"`
+}
+
+type UpdatedBusiness struct {
+	BusinessName string `bson:"business_name"`
+	Location     string `bson:"location"`
+	PhoneNumber  string `bson:"phone_number"`
+	ID           int
+	NumLeads     int
+	NumResponse  int
+	NumAccepts   int
+}
+
+func main() {
+	fmt.Println("userSearchFiltering: accessing environment variable:- mongoUrl")
+	mongoUrl := os.Getenv("mongoUrl")
+	// mongoUrl = "mongodb://developer:thedatabaseboss@cluster0.txuwl.mongodb.net/Bino_search?retryWrites=true&w=majority"
+	if mongoUrl == "" {
+		fmt.Println("Please set environment variable: mongoUrl to the url of mongoDB")
+		return
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoUrl)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -38,63 +68,235 @@ func main() {
 
 	fmt.Println("Connected to MongoDB!")
 
-	database := client.Database("TestDB")
+	database := client.Database("Bino_search")
 	fmt.Printf("Database name: %s\n", database.Name())
-    condition := bson.M{
-        "state": "business_send",
-        "business_details": bson.M{"$exists": true, "$not": bson.M{"$size": 0}},
-    }
+	condition := bson.M{
+		"state":            "business_send",
+		"business_details": bson.M{"$exists": true, "$not": bson.M{"$size": 0}},
+	}
 
-    // options to the search query of mongo
-    // findOptions := options.Find()
-    // findOptions.SetLimit(1000)
+	// options to the search query of mongo
+	// findOptions := options.Find()
+	// findOptions.SetLimit(1000)
 
-    cur, err := database.Collection("test_collection").Find(context.Background(), condition)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    var data []bson.M
-    if err := cur.All(context.Background(), &data); err != nil {
+	cur, err := database.Collection("users").Find(context.Background(), condition)
+	if err != nil {
 		log.Fatal(err)
 	}
 
+	var data []Mongo
+	if err := cur.All(context.Background(), &data); err != nil {
+		log.Fatal(err)
+	}
 
-    var output []bson.M
-    // Use Regex to match for travel, travelling etc
-    regex := `(?i)\btravel(?:ling|led)?\b`
-    for _, val := range data {
-        conv_start, ok := val["conv_start_msg"]
-        if ok {
-            str, yay := conv_start.(string)
-            if yay {
-                if ok, err := regexp.Match(regex, []byte(str)); ok{
-                    output = append(output, val)
-                } else if err != nil {
-                    fmt.Println("Failed to match regex error")
-                }
+	// disconnect from mongo
+	func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
+		fmt.Println("Disconnected from MongoDB!")
+	}()
 
-            }
-        }
-    }
+	// var output []bson.M
+	output := make(map[string]Business)
+	// Use Regex to match for travel, travelling etc
+	regex := `(?i)\btravel(?:ling|led)?\b`
 
-    // Marshalling json
-    out, err := json.Marshal(output)
-    if err != nil {
-        log.Fatalf("error marshalling")
-    }
+	for _, val := range data {
+		if ok, _ := regexp.Match(regex, []byte(val.ConvStartMsg)); ok {
+			businessList := val.BusinessDetails
+			for _, business := range businessList {
+				phone := business.PhoneNumber
+				_, ok := output[phone]
+				if !ok {
+					output[phone] = business
+				}
+			}
+		}
+	}
 
-    // Write marshalled json to file
-    err = os.WriteFile("output.json", out, 0644)
+	fmt.Printf("Num output %v\n", len(output))
+	total := len(output)
+	// Call bowApi
+	updatedBusinessMap := make(map[string]UpdatedBusiness)
+	fmt.Println("Calling bowApi to get business_id from phoneNum")
+	flag := 0
+
+	// add progress bar
+	bar := progressbar.Default(int64(total))
+	for number := range output {
+		id := getBowIDFromPhoneNum(number)
+		if id == 0 {
+			continue
+		}
+		// fmt.Println(id)
+		leads := getCountFromMongo("Bino_search", "broadcasts", "business_user_id", strconv.Itoa(id))
+		leadsRepliedTo := getCountFromMongo("Bino_search", "convreplies", "replied_business_id", strconv.Itoa(id))
+		leadsAccepted := getLeadsAccepted(id)
+
+		var temp UpdatedBusiness
+		temp.BusinessName = output[number].BusinessName
+		temp.Location = output[number].Location
+		temp.PhoneNumber = output[number].PhoneNumber
+		temp.ID = id
+		temp.NumLeads = leads
+		temp.NumResponse = leadsRepliedTo
+		temp.NumAccepts = leadsAccepted
+
+		updatedBusinessMap[number] = temp
+		// fmt.Printf("leads: %v\n", leads)
+		// fmt.Printf("replies: %v\n", leadsRepliedTo)
+		// fmt.Printf("accepts: %v\n", leadsRepliedTo)
+		flag++
+		bar.Add(1)
+	}
+
+	// Marshalling json
+	out, err := json.Marshal(updatedBusinessMap)
+	if err != nil {
+		log.Fatalf("error marshalling")
+	}
+
+	// Write marshalled json to file
+	err = os.WriteFile("output.json", out, 0644)
 	if err != nil {
 		fmt.Println("Error writing JSON to file:", err)
 		return
+	}
+
+}
+
+func getBowIDFromPhoneNum(phone string) (bowId int) {
+	url := fmt.Sprintf("https://app.bow.chat/api/v1/accounts/2/contacts/search?q=%v", phone)
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error generating request object")
+		return
+	}
+	apiKey := os.Getenv("APIKEY")
+	request.Header.Set("api_access_token", apiKey)
+
+	client := &http.Client{}
+	resp, error := client.Do(request)
+	if error != nil {
+		fmt.Println("error making request")
+		return
+	}
+
+	// Some phone numbers are not present in the database, hence, the api would return a 400. This has to ignored.
+	if resp.StatusCode != 200 {
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response: %v\n", err)
+		return
+	}
+
+	var respStruct RespStruct
+	json.Unmarshal(body, &respStruct)
+
+	if len(respStruct.Payload) == 0 {
+		return
+	}
+	bowId = respStruct.Payload[0].ID
+
+	return
+}
+
+func getCountFromMongo(db, collection, key, value string) (count int) {
+	mongoUrl := os.Getenv("mongoUrl")
+
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	database := client.Database(db)
+	condition := bson.M{
+		key: value,
+	}
+
+	// options to the search query of mongo
+	// findOptions := options.Find()
+	// findOptions.SetLimit(1000)
+	cnt, err := database.Collection(collection).CountDocuments(context.Background(), condition)
+	if err != nil {
+		log.Fatalf("Failed to count documents: %v", err)
 	}
 
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
 			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
 		}
-		fmt.Println("Disconnected from MongoDB!")
 	}()
+	count = int(cnt)
+
+	return
+}
+
+func getLeadsAccepted(id int) (count int) {
+	mongoUrl := os.Getenv("mongoUrl")
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	database := client.Database("Bino_search")
+	condition := bson.M{
+		"replied_business_id": strconv.Itoa(id),
+	}
+
+	cnt, err := database.Collection("convreplies").Find(context.Background(), condition)
+	if err != nil {
+		log.Fatalf("Failed to count documents: %v", err)
+	}
+
+	var data []ConvReplies
+	if err := cnt.All(context.Background(), &data); err != nil {
+		fmt.Println("error marshalling data")
+		fmt.Println(err)
+		return
+	}
+	num := 0
+	for _, reply := range data {
+		for _, svReply := range reply.ConvReply {
+			if svReply.ReplyContent == "Accept" {
+				num++
+				break
+			}
+		}
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
+	}()
+	count = num
+	return
 }
