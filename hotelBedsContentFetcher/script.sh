a5830513c033ca6a1522ba992e1ce352ee4094e8
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-01-30 11:24:04 +0530

    com

---
+++ hotelBedsContentFetcher/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export API="bd7ad5c1681b74b335f2960bce87805f"
+export SECRET="bfd826b5e8"
+export MONGOURL="mongodb://mongod:ubuntuMongo@20.197.0.216:27017/?authSource=admin"
+echo "Environment variables set"
